83eddcdfd6d01e27ae37cdf4ff7718ebfd69b7e0
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-11-19 23:49:40 +0100

    APP

--- resolvers/deleteCliente.ts
+++ resolvers/deleteCliente.ts
@@ -3,14 +3,15 @@
 
 import { Request, Response } from "npm:express@4.18.2";
 import ClienteModel from "../db/clientes.ts";
+import GestorModel from "../db/gestores.ts";
 
 // Esta función maneja una solicitud para eliminar un cliente.
 const deleteCliente = async (req: Request, res: Response) => {
   try {
-    // Obtiene el cif del cliente de los parámetros de la solicitud.
+    // Obtiene el dni del cliente de los parámetros de la solicitud.
     const { id } = req.params;
 
-    // Busca y elimina el cliente con el CIF otorgado.
+    // Busca y elimina el cliente con el dni otorgado.
     const Cliente = await ClienteModel.findOneAndDelete({ dni: id }).exec();
 
     // Si no encuentra cliente.
@@ -18,7 +19,23 @@ const deleteCliente = async (req: Request, res: Response) => {
       res.status(404).send("Cliente not found");
       return;
     }
+
+    const Gestor = await GestorModel.findOneAndDelete({ dni: Cliente.gestor }).exec();
+
+    if (!Gestor) {
+      res.status(404).send("Gestor not found");
+      return;
+    }
     
+    const updatedClientes = Gestor.clientes.filter((cliente: string) => cliente !== id);
+
+    await GestorModel.findOneAndUpdate(
+      // Buscamos un registro con 'dni' igual a gestor
+      { dni: Cliente.gestor },
+      // Actualizamos campos
+      { clientes: updatedClientes }
+    ).exec();
+
     // Caso contrario, se elimina correctamente, envía un mensaje de cliente eliminado.
     res.status(200).send("Cliente deleted");
 
